556ad7699cf4c34bf3c22b1597c2751584e70602
CaioAugustoHD <caioaugustosbs@gmail.com>
2023-02-02 11:50:52 -0300

    feat: Página Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { HighlightMovie } from "../../components/HighlightMovie";
+
+export function Home() {
+    return (
+        <>
+        <h1>Home</h1>
+        <HighlightMovie/>
+        </>
+    )
+
+}
